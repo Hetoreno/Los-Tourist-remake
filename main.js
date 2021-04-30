afac5bf3c9fb137bdc16c3cb00fa9e391e6cc8c8
Hetoreno <hectortamriel@gmail.com>
2021-04-29 21:21:18 -0700

    adding files

---
+++ main.js
@@ -0,0 +1,50 @@
+/* NAV bar */
+const burger = document.querySelector(".burger");
+const links = document.querySelector(".mobile");
+const navLinks = document.querySelectorAll(".mobile-link");
+
+burger.addEventListener("click",()=>{
+    links.classList.toggle("link-open");
+    burger.classList.toggle("toggle");
+});
+
+navLinks.forEach(navi =>{
+    navi.addEventListener("click",()=>{
+        links.classList.toggle("link-open");
+        burger.classList.toggle("toggle");
+    })
+})
+
+
+
+
+/* To-top arrow */
+const arrow = document.querySelector(".to-top");
+
+window.addEventListener("scroll",()=>{
+    window.pageYOffset > 800 ? arrow.classList.add("active") : arrow.classList.remove("active");
+});
+
+/* Gallery grid sort */
+const button = document.querySelectorAll(".list li");
+const list = document.querySelectorAll(".itembox");
+
+button.forEach((event) =>{
+    event.addEventListener("click", ()=> {
+
+        for (let i=0; i<button.length; i++){
+            button[i].classList.remove("active");
+        }
+        event.classList.add("active");
+
+        list.forEach((current)=>{
+            current.style.display="none";
+            let pictures = event.textContent.toLowerCase();
+            if(current.getAttribute("data-att")===pictures || pictures === "all"){
+                current.style.display="block";
+            }
+        })
+    })  
+})
+/* the 'data-att' needs to match with '.list li' names*/
+
